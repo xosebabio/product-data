6370ba003c71b594bc17df6305d120dfe777abb9
xbabio <xosebabiosabin@gmail.com>
2026-04-02 00:51:44 +0200

    Mongo entity

--- src/main/java/com/example/products/data/infrastructure/persistence/ProductMongodb.java
+++ src/main/java/com/example/products/data/infrastructure/persistence/ProductMongodb.java
@@ -2,10 +2,21 @@ package com.example.products.data.infrastructure.persistence;
 
 import lombok.Builder;
 import lombok.Value;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Document(collection = "products")
 @Value
 @Builder
 public class ProductMongodb {
+    @Id
+    UUID id;
+    String name;
+    String description;
+    Double price;
+    LocalDateTime createAt;
+    LocalDateTime updatedAt;
 }
